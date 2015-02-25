b0563b10bcfc555b886c00ac6a95404b15cbaedb
Logan Hanks <logan.hanks@gmail.com>
2015-02-24 20:25:18 -0800

    Fix examples.

--- examples_test.go
+++ examples_test.go
@@ -11,35 +11,35 @@ func ExampleBreakpointer() {
 	root := scope.New()
 
 	// A function that returns an error, which we want to simulate.
-	performSomeTask := func(arg string) (string, error) { return arg, nil }
+	output := func(arg string) error {
+		_, err := fmt.Println(arg)
+		return err
+	}
 
 	// A function that we want to test the error handling of.
-	errorHandler := func(ctx scope.Context, arg string) (string, error) {
-		if err := ctx.Check(performSomeTask, arg); err != nil {
-			return "", err
+	verifyOutput := func(ctx scope.Context, arg string) error {
+		if err := ctx.Check("output()", arg); err != nil {
+			return err
 		}
-		return performSomeTask(arg)
+		return output(arg)
 	}
 
-	// Set a breakpoint on a particular invocation of performSomeTask.
-	ctrl := root.Breakpoint(performSomeTask, "fail")
+	// Set a breakpoint on a particular invocation of output.
+	ctrl := root.Breakpoint("output()", "fail")
 
 	// Other invocations should proceed as normal.
-	_, err := errorHandler(root, "normal")
-	if err != nil {
-		fmt.Println("unexpected error:", err)
-	}
+	err := verifyOutput(root, "normal behavior")
+	fmt.Println("verifyOutput returned", err)
 
 	// Our breakpoint should allow us to inject an error. To control it
 	// we must spin off a goroutine.
 	go func() {
-		<-ctrl // synchronize at beginning of errorHandler
+		<-ctrl // synchronize at beginning of verifyOutput
 		ctrl <- fmt.Errorf("test error")
 	}()
 
-	if _, err := errorHandler(root, "fail"); err == nil || err.Error() != "test error" {
-		fmt.Println("unexpected result:", err)
-	}
+	err = verifyOutput(root, "fail")
+	fmt.Println("verifyOutput returned", err)
 
 	// We can also inject an error by terminating the context.
 	go func() {
@@ -47,26 +47,39 @@ func ExampleBreakpointer() {
 		root.Cancel()
 	}()
 
-	if _, err := errorHandler(root, "fail"); err != scope.Cancelled {
-		fmt.Println("unexpected result:", err)
-	}
+	err = verifyOutput(root, "fail")
+	fmt.Println("verifyOutput returned", err)
+
+	// Output:
+	// normal behavior
+	// verifyOutput returned <nil>
+	// verifyOutput returned test error
+	// verifyOutput returned context cancelled
 }
 
 func ExampleContext_cancellation() {
 	ctx := scope.New()
+
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(50 * time.Millisecond)
 		ctx.Cancel()
 	}()
 
+loop:
 	for {
-		t := time.After(time.Second)
+		t := time.After(10 * time.Millisecond)
 		select {
 		case <-ctx.Done():
-			break
+			break loop
 		case <-t:
 			fmt.Println("tick")
 		}
 	}
 	fmt.Println("finished with", ctx.Err())
+	// Output:
+	// tick
+	// tick
+	// tick
+	// tick
+	// finished with context cancelled
 }
